ae5a47b277b1b4e08a3d41a7d47c50e87841ca98
Vladislav Chabanov <chabanko@yandex.ru>
2017-09-22 15:11:48 +0300

    I slowly understand...

--- pascal.cpp
+++ pascal.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
+
 
 int main()
 {
-    // TODO: написать вывод треугольника Паскаля с использованием vector.
-    //
-    // Вид треугольника:
-    // 1
-    // 1 1
-    // 1 2 1
-    // 1 3 3 1
-    // 1 4 6 4 1
-    // ...
+    vector<vector<int> > v(7);
+    v[0].push_back(1);
+    v[1]=v[0]; v[1].push_back(1);
+    for (int i=2; i<v.size(); ++i)
+    {
+      v[i]=v[i-1];
+      v[i].push_back(1);
+      for (int g=0, j=1; (g<v[i-1].size()-1)& (j<v[i].size()); ++g,++j)
+          v[i][j]+=v[i-1][g];
+
+    }
+
+    for(int i=0;i<v.size();++i)
+    {
+      for (int j=0; j<v[i].size();++j)
+         cout << v[i][j] << ' ';
+      cout << endl;
+    }
     return 0;
 }
 
